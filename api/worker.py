fafa046612de1c93b5ace93371915e8dd51b28f0
Francisco Araujo <3943185+araujofrancisco@users.noreply.github.com>
2023-07-12 21:29:17 -0400

    Upgrade packages versions. Fixed issue with flower argument on monitor. Changed user and group for celery execution to avoid using root.

--- api/worker.py
+++ api/worker.py
@@ -6,4 +6,11 @@ CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
 
-celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+#celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery = Celery(__name__, 
+                broker=CELERY_BROKER_URL, 
+                backend=CELERY_RESULT_BACKEND,
+                broker_connection_retry=True,
+                broker_connection_retry_on_startup=True,
+                broker_connection_max_retries=10,
+                )
