fedd6641c94fdb4eda4b2703b3029135e0a75bb1
Francisco Araujo <3943185+araujofrancisco@users.noreply.github.com>
2023-07-14 11:26:29 -0400

    Remove commented code. Clean up dockerfiles.

--- api/worker.py
+++ api/worker.py
@@ -5,8 +5,6 @@ from celery import Celery
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
-
-#celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 celery = Celery(__name__, 
                 broker=CELERY_BROKER_URL, 
                 backend=CELERY_RESULT_BACKEND,
